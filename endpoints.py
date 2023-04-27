db5196c5fcd9112571ca52e4991aedd327b7021e
Maximuzon <maximuzon@gmail.com>
2023-04-28 00:29:41 +0600

    leshagay

--- endpoints.py
+++ endpoints.py
@@ -64,11 +64,12 @@ async def upload_image(place_id: int, file: UploadFile = File(...), db: Session 
     object_key = f"base/{filename}"
     print("set object key")
     print(object_key)
+    print(bucket_name)
     s3.upload_fileobj(file, bucket_name, object_key)
     print("file uploaded")
     # Update the existing record in the database with the new image URL
-
-    url = "https://{bucket_name}.fra1.digitaloceanspaces.com/{object_key}".format(bucket_name, object_key)
+    
+    url = "https://{0}.fra1.digitaloceanspaces.com/{1}".format(bucket_name, object_key)
     print(url)
     stmt = (update(Place).where(Place.place_id==place_id).values(images = url))
     db.execute(stmt)
