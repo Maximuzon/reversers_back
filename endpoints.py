debb22b6225b2af42ff9cba5239576465a0701a5
Maximuzon <maximuzon@gmail.com>
2023-05-01 22:28:45 +0600

    port fix

--- endpoints.py
+++ endpoints.py
@@ -373,5 +373,10 @@ async def get_all_tags(db: Session = Depends(get_db)):
     finally:
         db.close()
 
+# if __name__ == "__main__":
+#     uvicorn.run(app,host='0.0.0.0', port = 8000)
+
 if __name__ == "__main__":
-    uvicorn.run(app,host='0.0.0.0', port = 8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
