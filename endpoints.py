4ee2b5b517e738c08151233b52eb55c3708d1724
Maximuzon <maximuzon@gmail.com>
2023-05-31 23:37:41 +0600

    zxc

--- endpoints.py
+++ endpoints.py
@@ -52,7 +52,7 @@ async def root():
 
 # Dependency to get the database session
 def get_db():
-    db_url = "mysql://doadmin:AVNS_ixs6LYwnPYYNRCz3SRl@db-reversers-do-user-13881334-0.b.db.ondigitalocean.com:25060"
+    db_url = "mysql://doadmin:AVNS_ixs6LYwnPYYNRCz3SRl@db-reversers-do-user-13881334-0.b.db.ondigitalocean.com:25060/defaultdb?"
     engine = create_engine(db_url)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = SessionLocal()
